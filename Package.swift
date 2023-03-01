768336d2f76a7d0e8b52c9ba5e7cd2c9454a0edb
Enote AI <ai-ci-cd@enote.com>
2023-03-01 15:30:34 +0100

    Binary framework for verovio commit f8f251c99

--- Package.swift
+++ Package.swift
@@ -20,8 +20,8 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "VerovioFramework",
-                      url: "https://github.com/eNote-GmbH/Verovio-XCFramework/releases/download/v3.15.19/VerovioFramework.xcframework.zip",
-                      checksum: "16defccfc640c7ccb9d59d0c02f315cf03b1539e08346f7352730683c4c19bb3")
+                      url: "https://github.com/eNote-GmbH/Verovio-XCFramework/releases/download/v3.16.1/VerovioFramework.xcframework.zip",
+                      checksum: "a33210b86303e4247851d4c03e80541a3f9160b32dbe0064a79050536588155f")
     ]
 )
 
